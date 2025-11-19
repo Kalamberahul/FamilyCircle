18fd165aaafb62ba865eb2216e1bd70c78b04fb3
Kalamberahul <kalamberahul22@gmail.com>
2025-11-19 23:29:24 +0530

    PROJECT RESET: Fresh Spring Boot base installed to replace old broken code.

--- src/main/java/com/familycircle/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.familycircle.service;
-
-import com.familycircle.model.User;
-
-public interface UserService {
-    User findByUsername(String username);
-    void saveUser(User user);
-    void registerUser(User user);  // Include this
-}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.familycircle;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FamilyCircleApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
